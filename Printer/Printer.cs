07e1c863844cae835837470064c19a603bbeccb6
Mykola Kovshar <knikolay20031230@gmail.com>
2022-10-29 13:39:46 +0300

    Add Printer project

---
+++ Printer/Printer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class Printer
+    {
+        public virtual void Print(string value) => Console.WriteLine(value);
+
+    }
+}
